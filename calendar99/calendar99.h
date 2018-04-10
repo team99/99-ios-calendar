5471e3cc54b4ef0b6f9040284b223161d2483dae
Hai Pham <viethai.pham@holmusk.com>
2018-04-10 11:54:23 +0800

    Initial Commit

---
+++ calendar99/calendar99.h
@@ -0,0 +1,19 @@
+//
+//  calendar99.h
+//  calendar99
+//
+//  Created by Hai Pham on 10/4/18.
+//  Copyright © 2018 Hai Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for calendar99.
+FOUNDATION_EXPORT double calendar99VersionNumber;
+
+//! Project version string for calendar99.
+FOUNDATION_EXPORT const unsigned char calendar99VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <calendar99/PublicHeader.h>
+
+
